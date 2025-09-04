9e253a987f182561a1299f9ce74ad64239363787
Aleksadra Sakharova <Al.kikka@yandex.ru>
2025-09-04 21:36:35 +0300

    Исправлен локатор

--- src/main/java/pages/ConstructorPage.java
+++ src/main/java/pages/ConstructorPage.java
@@ -7,13 +7,12 @@ import org.openqa.selenium.WebElement;
 
 public class ConstructorPage extends BasePage {
 
-    // Локаторы вкладок
     private final By bunsTab = By.xpath("//span[text()='Булки']");
     private final By saucesTab = By.xpath("//span[text()='Соусы']");
     private final By fillingsTab = By.xpath("//span[text()='Начинки']");
 
-    // Локатор для поиска родительского контейнера вкладки
-    private final By tabParent = By.xpath("./ancestor::div[1]");
+    // локатор для проверки активного состояния вкладки
+    private final By activeParent = By.xpath("./parent::div[contains(@class,'tab_tab_type_current')]");
 
     public ConstructorPage(WebDriver driver) {
         super(driver);
@@ -39,6 +38,11 @@ public class ConstructorPage extends BasePage {
         driver.findElement(fillingsTab).click();
     }
 
+    private boolean isTabActive(By tabLocator) {
+        WebElement tab = driver.findElement(tabLocator);
+        return !tab.findElements(activeParent).isEmpty();
+    }
+
     @Step("Проверяем, что вкладка 'Булки' активна")
     public boolean isBunsActive() {
         return isTabActive(bunsTab);
@@ -52,13 +56,5 @@ public class ConstructorPage extends BasePage {
     @Step("Проверяем, что вкладка 'Начинки' активна")
     public boolean isFillingsActive() {
         return isTabActive(fillingsTab);
-    }
-
-    // 🔑 Вынесла повторяющуюся логику в приватный метод
-    private boolean isTabActive(By tabLocator) {
-        WebElement el = driver.findElement(tabLocator);
-        WebElement parent = el.findElement(tabParent);
-        String cls = parent.getAttribute("class");
-        return cls != null && cls.contains("tab_tab_type_current");
     }
 }
